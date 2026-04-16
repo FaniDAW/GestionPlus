12e1fb9250451c81ac897d8560123a8f3b8667d9
Fani <e_fani@hotmail.com>
2026-04-16 21:22:11 +0200

    Inicio proyecto

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import MiBoton from './components/MiBoton'
+
+export default function App() {
+  return (
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-blue-500 mb-4">
+          ¡Hola Tailwind! 🎉
+        </h1>
+        <MiBoton />
+      </div>
+  )
+}
---
+++ Frontend/src/components/Miboton.jsx
@@ -0,0 +1,7 @@
+export default function MiBoton() {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Haz clic aquí
+        </button>
+    )
+}
