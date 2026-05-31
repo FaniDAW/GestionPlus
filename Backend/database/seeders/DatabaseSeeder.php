c7065dac9835e442e4c7209fa751abd3c2c3eb8e
Fani <e_fani@hotmail.com>
2026-05-31 20:24:56 +0200

    Docker-compose y seeder (limpiar y mejorar)

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -3,11 +3,34 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        foreach ([
+            'personal_access_tokens',
+            'transactions',
+            'group_points',
+            'points',
+            'group_invitations',
+            'group_business',
+            'offers',
+            'rewards',
+            'invoices',
+            'subscriptions',
+            'groups',
+            'businesses',
+            'users',
+        ] as $table) {
+            DB::table($table)->truncate();
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
         $this->call([
             UserSeeder::class,
             BusinessSeeder::class,
